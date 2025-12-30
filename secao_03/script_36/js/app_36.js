286ffcc9e697c1dc56bf03dc7f85f8afff299098
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2025-12-30 09:41:32 -0400

    Extensões que utilizo no VSCode

---
+++ secao_03/script_36/js/app_36.js
@@ -0,0 +1,17 @@
+//Extensões que utilizo no VSCode
+
+
+/*Extensões
+    - Code Runner
+    - Code Time
+    - Django
+    - EJS language support
+    - Git Graph
+    - Python
+    - StandardJS JavaScript Standard Style
+    - Dracula official
+    - Material icon theme
+*/
+
+
+
